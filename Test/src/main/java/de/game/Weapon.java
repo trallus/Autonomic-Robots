d27082cd43420cd720a1628f40668562b7457dbf
Daniel Rajic <kontakt@danielrajic.de>
2014-05-27 10:27:32 +0200

    Weapon class' members added

--- Test/src/main/java/de/game/Weapon.java
+++ Test/src/main/java/de/game/Weapon.java
@@ -1,5 +1,64 @@
 package de.game;
 
 public class Weapon {
+	
+	private int range;
+	private int damage;
+	private int rateOfFire;
+	
+	
+	/**
+	 * 
+	 * @param range
+	 * @param damage
+	 * @param rateOfFire
+	 */
+	public Weapon(int range, int damage, int rateOfFire) {
+		super();
+		this.range = range;
+		this.damage = damage;
+		this.rateOfFire = rateOfFire;
+	}
+
+
+	//Getter und Setter: 
+	
+	public int getRange() {
+		return range;
+	}
+
+
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+
+
+	public int getDamage() {
+		return damage;
+	}
+
+
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+
+
+	public int getRateOfFire() {
+		return rateOfFire;
+	}
+
+
+
+	public void setRateOfFire(int rateOfFire) {
+		this.rateOfFire = rateOfFire;
+	}
+	
+	
+	
+	
 
 }

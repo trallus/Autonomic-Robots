70a48241eed83baae827c1770e31e346e3ca3687
mike <mikekiekebusch@gmail.com>
2014-10-29 17:39:17 +0100

    Changed WeaponPrototype so that it values are fixed on 1

--- Test/src/main/java/de/game/weapon/WeaponPrototype.java
+++ Test/src/main/java/de/game/weapon/WeaponPrototype.java
@@ -5,7 +5,8 @@ import de.math.Vector2D;
 public class WeaponPrototype extends Weapon {
 
     public WeaponPrototype(long range, long rateOfFire, long damage) {
-	super((int) range, (int) rateOfFire,(int) damage);
+	super(1,1,1); //TODO fixen und untere Zeile wieder herstellen
+	//super((int) range, (int) rateOfFire,(int) damage);
 	//Why is this long and not int like it is Declared in UML???
     }
 

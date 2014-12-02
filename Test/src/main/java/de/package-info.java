0be3cad1fc9a372bc848c5ec9f49623bb9e68dc1
mike <mikekiekebusch@gmail.com>
2014-12-02 14:30:07 +0100

    added a simple package documentation for package de

---
+++ Test/src/main/java/de/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The root package that hold all packages and classes of this application.
+ * @author Christian Köditz, Mike Kiekbusch
+ * @version 0.1
+ * @since the beginning
+ */
+package de;

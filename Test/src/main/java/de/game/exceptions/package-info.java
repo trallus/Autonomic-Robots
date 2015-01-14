be4787a3fc7f3b7d8b248a74d9e48e9a67a1d501
Christian <m@mail.com>
2015-01-14 09:39:21 +0100

    Add some package-info.java Files

---
+++ Test/src/main/java/de/game/exceptions/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the exceptions for the game
+ * @author Christian Köditz
+ * @version 0.6
+ * @since 29.10.2014
+ */
+package de.game.exceptions;
---
+++ Test/src/main/java/de/httpServer/ClientClasses/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the Client classes for the Server
+ * @author Christian Köditz
+ * @version 0.6
+ * @since 29.10.2014
+ */
+package de.httpServer.ClientClasses;
